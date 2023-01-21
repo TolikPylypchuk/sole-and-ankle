bcc463fede7af32758de4a61c85670e5de9aa22a
Tolik Pylypchuk <pylypchuk.tolik@gmail.com>
2023-01-21 23:33:30 +0200

    Style the super header

--- src/components/SuperHeader/SuperHeader.js
+++ src/components/SuperHeader/SuperHeader.js
@@ -26,16 +26,22 @@ const Wrapper = styled.div`
   font-size: 0.875rem;
   color: ${COLORS.gray[300]};
   background-color: ${COLORS.gray[900]};
+  height: 40px;
+  display: flex;
+  align-items: center;
+  padding-inline: 32px;
 `;
 
 const MarketingMessage = styled.span`
   color: ${COLORS.white};
+  margin-inline-end: auto;
 `;
 
 const HelpLink = styled.a`
   color: inherit;
   text-decoration: none;
   outline-offset: 2px;
+  margin-inline: 24px;
 
   &:not(:focus-visible) {
     outline: none;
